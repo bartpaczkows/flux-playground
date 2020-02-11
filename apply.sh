47aab427d25c18dfdc92b9455948513eae4858cf
bartpaczkows <54983956+bartpaczkows@users.noreply.github.com>
2020-02-11 15:42:32 +0100

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -6,7 +6,7 @@ set -u
 # Export any form of kubectl command that you use to interact with your cluster into KUBECTL_COMMAND.
 # It can be as simple as
 #
-#   export KUBECTL_COMMAND="kubectl"
+export KUBECTL_COMMAND="kubectl"
 #
 # or if you are using contexts, e.g.:
 #
